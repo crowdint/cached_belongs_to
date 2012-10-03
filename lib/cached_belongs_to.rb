29387b6774223898a9ae4637147d6bd12e0a6f2b
David Padilla <david@crowdint.com>
2012-10-02 22:32:04 -0500

    Use the association to get the callback variables

--- lib/cached_belongs_to.rb
+++ lib/cached_belongs_to.rb
@@ -16,16 +16,16 @@ module CachedBelongsTo
     #
     def cached_belongs_to(*args)
       caches = Array(args[1].delete(:caches))
-      klass  = args[0]
 
       association = belongs_to(*args)
       children_callback_name = "cached_belongs_to_#{name.demodulize.underscore}_callback".to_sym
-      create_cached_belongs_to_child_callbacks(caches, klass, children_callback_name)
-      create_cached_belongs_to_parent_callbacks(caches, klass, children_callback_name, association)
+      create_cached_belongs_to_child_callbacks(caches, children_callback_name, association)
+      create_cached_belongs_to_parent_callbacks(caches, children_callback_name, association)
     end
 
     private
-    def create_cached_belongs_to_child_callbacks(caches, klass, children_callback_name)
+    def create_cached_belongs_to_child_callbacks(caches, children_callback_name, association)
+      klass = association.name
       define_method children_callback_name do
         caches.each do |attr|
           send("#{klass}_#{attr}=", send(klass).send(attr)) if send(klass)
@@ -35,7 +35,8 @@ module CachedBelongsTo
       before_save children_callback_name
     end
 
-    def create_cached_belongs_to_parent_callbacks(caches, parent_class_name, children_callback_name, association)
+    def create_cached_belongs_to_parent_callbacks(caches, children_callback_name, association)
+      parent_class_name = association.name
       method_name = "cached_belongs_to_#{parent_class_name}_callback".to_sym
       has_many_association = self.name.demodulize.underscore.pluralize.to_sym
       # What is this? I don't even...
